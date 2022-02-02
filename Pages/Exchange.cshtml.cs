f093b3deefe86635a0556fe1dc1d6216d37604f5
Bronxy YO <fre-osa@mail.ru>
2022-02-02 16:53:19 +0700

    Exchange deleted

--- Pages/Exchange.cshtml.cs
+++ Pages/Exchange.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
 namespace WebApplication2.Pages
 {
     public class ExchangeModel : PageModel
